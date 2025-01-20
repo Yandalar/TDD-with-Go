7c77783d2365a17616d1b326d96761af46e8f3a6
Acosta, Pedro - Contractor <peacosta@contractor.ea.com>
2025-01-20 09:57:31 -0300

    Final refactoring

--- hello-go/hello.go
+++ hello-go/hello.go
@@ -1,16 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish    = "Spanish"
+	french     = "French"
+	portuguese = "Portuguese"
 
-func Hello(name string) string {
+	englishHelloPrefix    = "Hello, "
+	spanishHelloPrefix    = "Hola, "
+	frenchHelloPrefix     = "Bonjour, "
+	portugueseHelloPrefix = "Bom dia, "
+)
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case portuguese:
+		prefix = portugueseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Bruno"))
+	fmt.Println(Hello("Bruno", ""))
 }
